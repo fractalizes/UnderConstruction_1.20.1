705bd28c55f3366610b76fba126af26d197ad181
fractalizes <ibunsiri@ucsc.edu>
2025-11-04 15:35:38 -0800

    villager trading works TODO mess around with rng values

---
+++ src/main/java/net/blockboys/underconstruction/event/ModEvents.java
@@ -0,0 +1,42 @@
+package net.blockboys.underconstruction.event;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.blockboys.underconstruction.UnderConstruction;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = UnderConstruction.MOD_ID)
+public class ModEvents {
+
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+
+        if (event.getType() == VillagerProfession.FARMER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            // testing trades, it works!
+            // TODO find how to make trade guaranteed rather based off rng
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    new ItemStack(Items.CARROT, 10),
+                    10, 8, 0.02f
+            ));
+
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(Items.PUMPKIN_SEEDS, 3),
+                    4, 15, 0.03f
+            ));
+
+        }
+    }
+
+}
