b27b6b904f01c2cd01eff0bf7c1c2771ed023883
ivli18 <161982162+ivli18@users.noreply.github.com>
2025-11-19 18:33:03 -0800

    Add initial structure generation

---
+++ src/main/java/net/blockboys/underconstruction/structure/SpawnStructure.java
@@ -0,0 +1,31 @@
+package net.blockboys.underconstruction.structure;
+
+import net.blockboys.underconstruction.UnderConstruction;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
+import net.minecraftforge.event.server.ServerStartedEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = UnderConstruction.MOD_ID)
+public class SpawnStructure {
+
+    @SubscribeEvent
+    public static void onServerStart(ServerStartedEvent event) {
+        ServerLevel level = event.getServer().overworld(); // Overworld only
+        BlockPos spawn = level.getSharedSpawnPos();        // World spawn
+
+        StructureTemplateManager manager = level.getStructureManager();
+        ResourceLocation id = new ResourceLocation("under_construction:ruins");
+        StructureTemplate template = manager.getOrCreate(id);
+
+        if (template != null) {
+            StructurePlaceSettings settings = new StructurePlaceSettings();
+            template.placeInWorld(level, spawn, spawn, settings, level.random, 2);
+        }
+    }
+}
